3d81b080dbfc1da9cd74bd16a34f8c42a0c81599
Billamw <66533754+Billamw@users.noreply.github.com>
2023-02-11 21:36:53 +0100

    made it work again

--- p5/empty-example/sketch.js
+++ p5/empty-example/sketch.js
@@ -1,7 +1,7 @@
-let xMin = -5;
-let xMax = 5;
-let yMin = -4;
-let yMax = 6;
+let xMin = -4;
+let xMax = 4;
+let yMin = -3;
+let yMax = 5;
 
 let t0 = 0;
 let t02 = 0;
@@ -13,15 +13,21 @@ let b1 = t0 + T/2;
 let a2 = t0 - T/2;
 let b2 = t02 + T2/2; 
 
+let functionInput;
+
 let impuls1 = rectImpuls;
-let impuls2 = pralbolaImpuls;
+let impuls2 = paralbolaImpuls;
 
 function setup() {
   createCanvas(600, 600);
 
-  slidert0 = createSlider(xMin, xMax, 0, 0.1);
+  slidert0 = createSlider(xMin, xMax, -2, 0.05);
   slidert0.style('width', '300px')
-  sliderT = createSlider(0.1, 4, 1, 0.1);
+  sliderT = createSlider(1, 3, 1, 1);
+
+  functionInput = createInput("x*x");
+  functionInput.position(20,20);
+
   stroke(0);
   noFill();
 }
@@ -37,21 +43,18 @@ function draw() {
   drawImpuls(impuls2, t02, T2);
 
   stroke(255,0,0);
-  beginShape();
-  for (let x = xMin; x <= t0; x += 0.01) {
-    let y = 0;
-    //y = borders();
-    y = convolution(impuls1, impuls2, x);
-    //point(t0, y);
-    point(map(x, xMin, xMax, 0, width), map(y, yMin, yMax, height, 0));
-  }
-  endShape();
+  strokeWeight(2);
+
+  drawConvolution(impuls1, impuls2);
+
+  strokeWeight(1);
   stroke(0);
 
-  text('a1', map(a1, xMin, xMax, 0, width), 390);
-  text('b1', map(b1, xMin, xMax, 0, width), 370);
-  text('a2', map(a2, xMin, xMax, 0, width), 370);
-  text('b2', map(b2, xMin, xMax, 0, width), 390);
+  // text('a1', map(a1, xMin, xMax, 0, width), 390);
+  // text('b1', map(b1, xMin, xMax, 0, width), 370);
+  // text('a2', map(a2, xMin, xMax, 0, width), 370);
+  // text('b2', map(b2, xMin, xMax, 0, width), 390);
+
 }
 
 
@@ -71,11 +74,22 @@ function triangularImpuls(t) {
   }
 }
 
-function pralbolaImpuls(t) {
+function paralbolaImpuls(t) {
   if(abs(t)<=0.5){
     return (t-.5)*(t-.5);
   }
   return 0;
+}
+
+// trying to do a lifetime function Input
+function evalFunction(x) {
+  let f = functionInput.value();
+
+  if(abs(x)<=0.5){
+    return eval(f);
+  }
+  return 0;
+
 }
 
 function drawImpuls(impuls, t0, T) {
@@ -87,98 +101,38 @@ function drawImpuls(impuls, t0, T) {
   endShape();
 }
 
-function convolution(signal1, signal2, time) {
-  let result = 0;
-  // for (let tau = -time; tau <= time; tau += 0.01) {
-  //   result += signal1(tau) * signal2(time - tau);
-  // }
-
-  // if(a1 > b1) {
-  //   result = 0 
-  // } 
-  if(b1 > b2) {
-    //result = integral(impuls1, a1, b1) * integral(impuls2, a1, b1);
-    //result = integral(convoFunc(time),a1, b1); //Ich muss noch mal verstehen wie die Faltung rechnerisch wirklich funktioniert.
-    result = integralConvo(signal1, signal2, a1, b1, time);
-  } 
-  else if(b2 > b1) {
-    //result = integral(impuls1, a2, b1) * integral(impuls2, a2, b1);
-    //result = integral(convoFunc(time),a2, b1);
-    result = integralConvo(signal1, signal2, a2, b1, time);
-  } 
-  else if(b2 > a2) {
-    //result = integral(impuls1, a2, b2) * integral(impuls2, a2, b2);
-    result = integralConvo(signal1, signal2, a2, b2, time);
-  } 
-  // else if(a2 > b2) {
-  //   result = 0;
-  // }
-  else{
-    result = 0;
-  }
-  
-  return result;
-}
-
-function borders() {
+function drawConvolution(impuls1, impuls2) {
   let y = 0;
-  if(a1 > b1) {
-   y = 0 
-  } else if(a1 > b2) {
-    y = integral(impuls1, a1, b1) * integral(impuls2, a1, b1);
-  } else if(b2 > b1) {
-    y = integral(impuls1, a2, b1) * integral(impuls2, a2, b1);
-  } else if(b2 > a2) {
-    y = integral(impuls1, a2, b2) * integral(impuls2, a2, b2);
-  }  else if(a2 > b2) {
-    y = 0;
+  beginShape();
+  for (let x = xMin; x <= t0; x += 0.01) {
+    y = convolution(impuls1, impuls2, x);
+    vertex(map(x, xMin, xMax, 0, width), map(y, yMin, yMax, height, 0));
   }
-  return y;
+  endShape();
 }
 
+function convolution(impuls1, impuls2, t) {
+  let a = a1;
+  let b = b1;
+  let tau1, tau2, y1, y2;
+  let dtau = 0.001;
+  let result = 0;
 
+  tau1 = a;
+  tau2 = a + dtau;
 
-// calculating the integral of a function f in range of [a; b]
-function integral(f, a, b) {
-  
-  let x1, x2, y1, y2;
-  let dx = 0.001;
-  let sum = 0;
+  // making sub rectangles for approximatin overlapping area
+  for (let i = a; i < b; i+= dtau) {
 
-  x1 = a;
-  x2 = a + dx;
+    // continous convolution
+    y1 = impuls1(tau1 / T) * impuls2((t-tau1)/ T);
+    y2 = impuls1(tau2 / T) * impuls2((t-tau2)/ T2);
 
-  for (let i = a; i < b; i+= dx) {
-    y1 = f(x1);
-    y2 = f(x2);
-    sum += (x2 - x1) * (y1 + y2) / 2;
-    x1 = x2;
-    x2 = x1 + dx;
+    // calculating the area of a sub rectangle
+    result += dtau * (y1 + y2) / 2;
+    tau1 = tau2;
+    tau2 = tau1 + dtau;
   }
 
-  return sum;
-}
-
-function integralConvo(f1, f2, a, b, time) {
-  
-  let x1, x2, y1, y2;
-  let dx = 0.001;
-  let sum = 0;
-
-  x1 = a;
-  x2 = a + dx;
-
-  for (let i = a; i < b; i+= dx) {
-    y1 = f1(x1) * f2(time-x1);
-    y2 = f1(x2) * f2(time-x2);
-    // y1 = f1( (x1 - t0) / T) * f2( ((time-x1) - t02) / T2);
-    // y2 = f1( (x2 - t0) / T) * f2( ((time-x2) - t02) / T2);
-
-    //calculating the area of a sub rectangle
-    sum += dx * (y1 + y2) / 2;
-    x1 = x2;
-    x2 = x1 + dx;
-  }
-
-  return sum;
+  return result;
 }
