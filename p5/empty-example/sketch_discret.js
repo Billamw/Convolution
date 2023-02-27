181cd930e2ff65a1996d876750bcd4d78eb60e9e
Billamw <66533754+Billamw@users.noreply.github.com>
2023-02-27 13:56:12 +0100

    .

--- p5/empty-example/sketch_discret.js
+++ p5/empty-example/sketch_discret.js
@@ -1,13 +1,13 @@
 let xMin = -13;
 let xMax = 13;
-let yMin = -10;
-let yMax = 10;
+let yMin = -7;
+let yMax = 13;
 
 // let signal1 = [.3,.6,.7,.8,.9];
 // let signal2 = [.9,.8,.7,.8];
 
-let signal1 = [1,1.5,2,2.5];
-let signal2 = [1,1.5,2,2.5];
+let signal1 = [1,1.2,1.4,1.6,1,.5];
+let signal2 = [1,1.2,1.4,1.6];
 
 let t0 = 0;
 let t02 = 0-signal2.length/2;
@@ -16,7 +16,7 @@ let t02 = 0-signal2.length/2;
 function setup() {
   createCanvas(600, 600);
 
-  slidert0 = createSlider(xMin+1, xMax-4, 0, 1);
+  slidert0 = createSlider(xMin+1, xMax-signal1.length, -(signal1.length + signal2.length/2 + 1), 1);
   slidert0.style('width', '300px')
 
   stroke(0);
@@ -28,25 +28,25 @@ function draw() {
   background(255);
   
   t0 = slidert0.value();
-  drawSignal(signal1, t0);
+  drawxt(signal1, t0);
   drawht(signal2, t02);
   drawDiscretConvolution(signal1, signal2, t02);
 
 }
 
-function drawSignal(signal, t) {
-    let y, tmp;
-    for (let x = 0; x < signal.length; x++) {
-      if(signal[x] == 0) {
-        continue;
-      }
-      tmp = map(x+t, xMin, xMax, 0, width);
-      y = map(signal[x], yMin, yMax, height, 0)
-      strokeWeight(5);
-      point(tmp,y);
-      strokeWeight(2);
-      line(tmp, map(0, yMin, yMax, height, 0), tmp, y);
+function drawxt(signal, t) {
+  let y, x1;
+  for (let x = 0; x < signal.length; x++) {
+    if(signal[x] == 0) {
+      continue;
     }
+    x1 = map(x+t, xMin, xMax, 0, width);
+    y = map(signal[x], yMin, yMax, height, 0)
+    strokeWeight(5);
+    point(x1,y);
+    strokeWeight(2);
+    line(x1, map(0, yMin, yMax, height, 0), x1, y);
+  }
 }
 function drawht(signal, t) {
     let y, tmp;
@@ -65,7 +65,7 @@ function drawht(signal, t) {
 
 function drawDiscretConvolution(signal1, signal2, t)  {
   stroke('red');
-  drawSignal(discreteConvolution(signal1, signal2), t)
+  drawxt(discreteConvolution(signal1, signal2), t)
   stroke('black')
 }
 
